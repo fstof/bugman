a279c5d584cc77c13049e6c1f4709abafa194c4d
Frans Stofberg <frans.stofberg@gmail.com>
2021-11-26 21:18:51 +0200

    preload audio cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bonfire/bonfire.dart';
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,7 +21,17 @@ void main() async {
     await Flame.device.setLandscape();
     await Flame.device.fullScreen();
   }
-
+  await FlameAudio.audioCache.loadAll([
+    'bleeps/collect.wav',
+    'bleeps/die.wav',
+    'bleeps/get-can.wav',
+    'bleeps/kill.wav',
+    'bleeps/power-up.wav',
+    'bleeps/spray.wav',
+    'music/level_complete.wav',
+    'music/level_play.wav',
+    'music/level_start.wav',
+  ]);
   runApp(const MyApp());
 }
 
